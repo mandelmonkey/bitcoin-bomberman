f3b0918766e6ba0d7640443cac5d9a3c4d3b4832
BigBossRabbit <87260669+BigBossRabbit@users.noreply.github.com>
2023-07-30 15:22:45 +0200

    Adding Paywall.js and Withdrawl.js files
    I could not get my copy of VSCodium to upload my changes via the synch functionaity so I have uploaded them directly. This is as a submission for the AI4All Bounty: 2/3 AI4ALL: Build a Kaboom Game with an AI Enemy and Bitcoin Payments using the ZBD API

--- js/Paywall.js
+++ js/Paywall.js
@@ -22,11 +22,68 @@ function startGame(){
 }
 
 function createInvoice() {
-    
+     
+    const Http = new XMLHttpRequest();
+    const url = 'https://api.zebedee.io/v0/charges';
+
+    Http.open("POST", url);
+
+    Http.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    Http.setRequestHeader("apikey", config.API_KEY);
+
+    const payload = JSON.stringify({
+        "expiresIn": 300,
+        "amount": config.FEE * 1000,
+        "description": "EasySats - Bomberman Paywall",
+        "internalId": "11af01d092444a317cb33faa6b8304b8",
+        "callbackUrl": "https://your-website.com/callback"
+    })
+    Http.send(payload);
+
+    Http.onreadystatechange = (e) => {
+
+        if (Http.readyState === XMLHttpRequest.DONE) { 
+            console.log(Http.responseText)
+            const data = JSON.parse(Http.responseText);
+            currentChargeID = data.data.id;
+            createInvoiceQRCode(data);
+        }
+
+    }
 }
 
 function checkPayment() {
-     
+    const Http = new XMLHttpRequest();
+    const url = 'https://api.zebedee.io/v0/charges/' + currentChargeID;
+
+    Http.open("GET", url);
+
+    Http.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    Http.setRequestHeader("apikey", config.API_KEY);
+
+    Http.send();
+
+    Http.onreadystatechange = (e) => {
+
+        if (Http.readyState === XMLHttpRequest.DONE) { 
+            console.log(Http.responseText);
+            const data = JSON.parse(Http.responseText);
+            const status = data.data.status;
+
+            if (status === "pending") {
+
+                setTimeout(function() {
+                    checkPayment();
+                }, 2000);
+
+            } else if (status === "completed") {
+
+               startGame();
+            
+            }
+        }
+    }
+
 }
 
 createInvoice();
